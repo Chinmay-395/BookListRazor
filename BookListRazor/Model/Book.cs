012521a72e2c137aec3d6e46cb2814ac482257cf
Chinmay-395 <chinmaydali395@gmail.com>
2021-07-30 12:53:50 +0530

    Add project files.

---
+++ BookListRazor/Model/Book.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookListRazor.Model
+{
+    public class Book
+    {
+        /* [Key] ---> this will automatically add a 'id' field in the database */
+        [Key]
+        public int Id { get; set; }
+        /* [Required] ---> this attribute cannot be null */
+        [Required]
+        public string Name { get; set; }
+        public string Author { get; set; }
+    }
+}
